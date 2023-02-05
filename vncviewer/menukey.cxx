4911e66cc85430a65dca07329ae8b32de5d895c7
geoboom <leegeorgie52@gmail.com>
2023-02-05 14:31:41 +0800

    Add Control_R support for MenuKey

--- vncviewer/menukey.cxx
+++ vncviewer/menukey.cxx
@@ -1,16 +1,16 @@
 /* Copyright 2011 Martin Koegler <mkoegler@auto.tuwien.ac.at>
  * Copyright 2011 Pierre Ossman <ossman@cendio.se> for Cendio AB
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
@@ -54,6 +54,7 @@ static const MenuKeySymbol menuSymbols[] = {
   {"Home",        FL_Home,        0xc7, XK_Home},
   {"Page_Up",     FL_Page_Up,     0xc9, XK_Page_Up},
   {"Page_Down",   FL_Page_Down,   0xd1, XK_Page_Down},
+  {"Control_R",   FL_Control_R,   0x9d, XK_Control_R},
 };
 
 int getMenuKeySymbolCount()
